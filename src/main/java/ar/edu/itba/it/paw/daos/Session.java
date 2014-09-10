fb8adce364113ab06da0759c0fb8c4f066f18707
lbadi <leonelbadi@hotmail.com>
2014-09-10 00:59:56 -0300

    Saco comentarios inecesarios

--- src/main/java/ar/edu/itba/it/paw/daos/Session.java
+++ src/main/java/ar/edu/itba/it/paw/daos/Session.java
@@ -1,10 +1,8 @@
 package ar.edu.itba.it.paw.daos;
 
-//import java.lang.reflect.Constructor;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-//import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -12,7 +10,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
 
-//import ar.edu.itba.it.paw.models.Movie;
 
 public class Session<T> {
 	
@@ -22,13 +19,10 @@ public class Session<T> {
 	String dbname = "paw7";
 	Connection conn;
 
-	private Class<T> clazz;
-	//private List<String> returnFields;
 	private List<Criteria> criteria = new ArrayList<Criteria>();
 	
 	
 	public Session(Class<T> clazz){
-		this.clazz = clazz;
 		Properties connectionProps = new Properties();
 	    connectionProps.put("user",userName);
 	    connectionProps.put("password", password);
@@ -44,40 +38,20 @@ public class Session<T> {
 		this.criteria.add(criteria);
 	}
 	
-	
-	
-	
 	public ResultSet list(String table){
 		String query = "SELECT " + "* " + "FROM " + table;
 		if(criteria.size() > 0){
 			query = query + " WHERE ";
-			Iterator critIterator = criteria.iterator();
+			Iterator<Criteria> critIterator = criteria.iterator();
 			while(critIterator.hasNext()){
 				query = query + critIterator.next();
 				if(critIterator.hasNext())
 					query = query + " AND ";
 			}
-		
 		}
 		try {
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
-			/*Intento de hacer con Reflection para poder armar una clase directamente
-			ResultSetMetaData rsmd = rs.getMetaData();
-			int columnsNumber = rsmd.getColumnCount();
-			Constructor<T>[] constructors = (Constructor<T>[]) clazz.getConstructors();
-			
-			while(rs.next()){
-				clazz.getConstructor(parameterTypes)
-				rs.getMetaData().
-				String name = rs.getString("nombre");
-				String description = rs.getString("descripcion");
-				Long code = rs.getLong("codigo");
-				T object = new T(code, name, description);
-				
-				movies.add(movie);
-			}
-			 */
 			return rs;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
